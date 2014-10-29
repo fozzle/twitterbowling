f8010628d3ffc61ff427192668f76523e06f66df
Kyle Petrovich <kylepetrovich@gmail.com>
2014-10-28 21:13:31 -0400

    twitter bowling 0.1

---
+++ botbowl.js
@@ -0,0 +1,60 @@
+(function() {
+  var Entry = Parse.Object.extend("Entry");
+  var leaders = document.getElementById("leaders");
+  var bowlingForm = document.getElementById("bowl");
+
+  bowl.addEventListener("submit", newEntry);
+
+  populateLeaderboard();
+
+  function newEntry(e) {
+    e.preventDefault();
+
+    var entry,
+      tweet = document.getElementById("tweet"),
+      nickname = document.getElementById("nickname");
+
+    if (!tweet.value || !nickname.value) {
+      alert("Please fill out the entire form to enter.");
+      return;
+    }
+
+    entry = new Entry();
+    entry.set("text", tweet.value);
+    entry.set("nickname", nickname.value);
+    entry.save();
+
+    tweet.value = "";
+    nickname.value = "";
+    alert("Thanks for playing.")
+  }
+
+
+  function populateLeaderboard() {
+    var query = new Parse.Query(Entry);
+    query.descending("score");
+    query.limit(20);
+    query.greaterThan("score", 0);
+    query.find({
+      success: function(results) {
+        if (!results.length) {
+          leaders.innerHTML = "Awaiting our champion...";
+        }
+        var fragment = document.createDocumentFragment(),
+          li;
+        for (var i = 0; i < results.length; i++) {
+          li = document.createElement("li");
+          li.innerHTML = "Entrant: " + results[i].get("nickname") + " Score: " + results[i].get("score");
+          fragment.appendChild(li);
+        }
+
+        leaders.appendChild(fragment);
+      },
+      error: function(err) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+  }
+
+
+})();
